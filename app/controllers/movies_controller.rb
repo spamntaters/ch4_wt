8b9748f4d409f00e08b56062c4f7a6205c3de443
spamntaters <smurra28@uncc.edu>
2017-10-09 15:28:05 -0400

    Completed section 4.10

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,11 +22,13 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
+  @movie = Movie.find params[:id]
+  @movie.update_attributes!(movie_params)
+  respond_to do |client_wants|
+    client_wants.html {  redirect_to movie_path(@movie)  } # as before
+    client_wants.xml  {  render :xml => @movie.to_xml    }
   end
+end
 
   def destroy
     @movie = Movie.find(params[:id])
